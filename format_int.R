8bf2f5af065805137ff4d7e0fdd231434973e6fe
burnsal <amiraburns@gmail.com>
2026-05-28 10:23:04 -0600

    raw and formatted interest form responses

---
+++ format_int.R
@@ -0,0 +1,62 @@
+### format interest form responses ###
+
+# load data
+pref_raw <- readr::read_csv("bookclub_pref_raw.csv")
+
+## genre preferences
+
+# create and lengthen df
+read_df <- pref_raw |>
+  dplyr::select(-Trigger) |>
+  tidyr::separate_longer_delim(Genre, delim = ", ")
+# subgenre as separate column
+read_df <- read_df |>
+  tidyr::separate_wider_delim(Genre, " - ", names = c("Genre", "Subgenre"),
+                              too_few = "align_start")
+# tidy missing values
+read_df <- read_df |>
+  dplyr::mutate(Subgenre = dplyr::if_else(is.na(Subgenre), Genre, Subgenre)) |>
+  dplyr::mutate(dplyr::across(Genre:Subgenre, ~stringr::str_to_title(.)))
+
+# fix sloppy nonfiction formats
+nonfics <- c("Arts", "Culture", "History", "Philosophy",
+             "Psychology", "Science", "Spirituality")
+read_df <- read_df |>
+  dplyr::mutate(Genre = dplyr::if_else(Subgenre %in% nonfics,
+                                       "Nonfiction", Genre))
+
+# fix sloppy fiction formats
+read_df <- read_df |>
+  dplyr::mutate(dplyr::across(Genre:Subgenre, ~dplyr::if_else(stringr::str_starts(Genre, "Sci"),
+                                                              "Science Fiction", .)))
+read_df <- read_df |>
+  dplyr::mutate(Subgenre = dplyr::if_else((Genre == "Romance" & Subgenre != "Romance"),
+                                          paste0("Romance - ", Subgenre), Subgenre)) |>
+  tidyr::separate_wider_delim(Subgenre, " - ", names = c("Subgenre", "Subsubgenre"),
+                              too_few = "align_start", too_many = "drop")
+subgens <- c("Fantasy", "Science Fiction", "Romance")
+read_df <- read_df |>
+  dplyr::mutate(Genre = dplyr::if_else(Subgenre %in% subgens,
+                                       "Fiction", Genre))
+
+# clean up nonstandard entries
+read_df <- read_df |>
+  dplyr::distinct() |>
+  dplyr::filter(Genre != "Etc") |>
+  dplyr::mutate(Genre = dplyr::if_else(nchar(gsub("[^ ]", "", Subgenre)) > 2,
+                                       "Other", Genre))
+
+save(read_df, file = "prefs.RData")
+
+# -- END
+
+## Triggers or Vetos
+
+# create and lengthen df
+noread_df <- pref_raw |>
+  dplyr::select(-Genre) |>
+  tidyr::separate_longer_delim(Trigger, delim = ", ")
+
+
+
+save(read_df, noread_df, file = "prefs.RData")
